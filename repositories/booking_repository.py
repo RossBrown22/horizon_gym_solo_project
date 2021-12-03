16ecb10f8f66f17817b0228b8aeb35db073cc4b2
Ross Brown <rossbrown22@hotmail.co.uk>
2021-12-03 13:51:06 +0000

    added booking repo

---
+++ repositories/booking_repository.py
@@ -0,0 +1,34 @@
+from db.run_sql import run_sql
+
+from models.booking import Booking
+import repositories.member_repository as member_repository
+import repositories.fitness_class_repository as fitness_class_repository
+
+def save(booking):
+    sql = "INSERT INTO bookings (member_id, fitness_class_id) VALUES (%s, %s) RETURNING id"
+    values = [booking.member.id, booking.fitness_class.id]
+    results = run_sql( sql, values )
+    booking.id = results[0]['id']
+    return booking
+
+def select_all():
+    bookings = []
+
+    sql = "SELECT * FROM bookings"
+    results = run_sql(sql)
+
+    for row in results:
+        member = member_repository.select(row['member_id'])
+        fitness_class = fitness_class_repository.select(row['fitness_class_id'])
+        booking = Booking(member, fitness_class, row['id'])
+        bookings.append(booking)
+    return bookings
+
+def delete_all():
+    sql = "DELETE FROM bookings"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM bookings WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,4 +1,5 @@
 from db.run_sql import run_sql
+
 from models.fitness_class import FitnessClass
 from models.member import Member
 
